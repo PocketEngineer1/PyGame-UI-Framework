9612e8205d9d9e2993052c5f5bba713a6b6c3627
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-04-07 08:58:27 -0500

    I forgot the event handler

--- UI_Framework.py
+++ UI_Framework.py
@@ -4,24 +4,42 @@ pygame.init()
 pygame.font.init()
 
 class TextInput:
-  def __init__(self, x, y, width, height, font_size=20, text=''):
+  def __init__(self, x, y, width, height, font_size=20, placeholder='', on_change=None):
     self.rect = pygame.Rect(x, y, width, height)
-    self.text = text
+    self.text = ''
     self.font = pygame.font.Font(None, font_size)
+    self.placeholder = placeholder
+    self.placeholder_color = (128, 128, 128)
+    self.text_color = (0, 0, 0)
+    self.active_color = (0, 0, 255)
+    self.inactive_color = (128, 128, 128)
     self.active = False
+    self.on_change = on_change
 
   def draw(self, surface):
-    pygame.draw.rect(surface, (255, 255, 255), self.rect, 2)
-    text_surface = self.font.render(self.text, True, (0, 0, 0))
-    surface.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
+    # Draw the text input box
+    pygame.draw.rect(surface, self.active_color if self.active else self.inactive_color, self.rect, 2)
+
+    # Draw the text if it exists
+    if self.text != '':
+      text_surface = self.font.render(self.text, True, self.text_color)
+      surface.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
+    # Draw the placeholder text if the input is empty
+    else:
+      placeholder_surface = self.font.render(self.placeholder, True, self.placeholder_color)
+      surface.blit(placeholder_surface, (self.rect.x + 5, self.rect.y + 5))
 
   def handle_event(self, event):
     if event.type == pygame.MOUSEBUTTONDOWN:
+      # Toggle the active state of the input if clicked on
       if self.rect.collidepoint(event.pos):
         self.active = not self.active
       else:
         self.active = False
-    if event.type == pygame.KEYDOWN:
+      # Update the text color based on the active state
+      self.text_color = (0, 0, 0) if self.active else (128, 128, 128)
+    elif event.type == pygame.KEYDOWN:
+      # Add characters to the input text if active
       if self.active:
         if event.key == pygame.K_RETURN:
           self.active = False
@@ -29,42 +47,34 @@ class TextInput:
           self.text = self.text[:-1]
         else:
           self.text += event.unicode
-          
-  def get_text(self):
-    return self.text
+        # Call the on_change function if it exists
+        if self.on_change is not None:
+          self.on_change(self.text)
 
 class Checkbox:
-  def __init__(self, x, y, size, color, border_width=2, checked=False, click_handler=None):
-    self.x = x
-    self.y = y
-    self.size = size
-    self.color = color
-    self.border_width = border_width
+  def __init__(self, x, y, size, label='', font_size=20, checked=False, color=(0, 0, 0), check_color=(255, 255, 255)):
+    self.rect = pygame.Rect(x, y, size, size)
+    self.label = label
+    self.font = pygame.font.Font(None, font_size)
     self.checked = checked
-    self.click_handler = click_handler
-  
+    self.color = color
+    self.check_color = check_color
+
   def draw(self, surface):
-    # Draw the checkbox border
-    pygame.draw.rect(surface, (0, 0, 0), (self.x, self.y, self.size, self.size), self.border_width)
-
-    # Draw the checkbox background
-    pygame.draw.rect(surface, self.color, (self.x + self.border_width, self.y + self.border_width, self.size - 2 * self.border_width, self.size - 2 * self.border_width))
-
-    # Draw the checkmark if the checkbox is checked
+    pygame.draw.rect(surface, self.color, self.rect, 2)
     if self.checked:
-      check_size = int(self.size * 0.6)
-      check_rect = pygame.Rect(self.x + self.size / 2 - check_size / 2, self.y + self.size / 2 - check_size / 2, check_size, check_size)
-      pygame.draw.rect(surface, (0, 0, 0), check_rect)
-  
+      pygame.draw.line(surface, self.check_color, (self.rect.left + 5, self.rect.centery), (self.rect.centerx, self.rect.bottom - 5), 2)
+      pygame.draw.line(surface, self.check_color, (self.rect.centerx, self.rect.bottom - 5), (self.rect.right - 5, self.rect.top + 5), 2)
+
+    if self.label:
+      label_text = self.font.render(self.label, True, self.color)
+      label_rect = label_text.get_rect(left=self.rect.right + 10, centery=self.rect.centery)
+      surface.blit(label_text, label_rect)
+
   def handle_event(self, event):
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      if self.clicked(event.pos):
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+      if self.rect.collidepoint(event.pos):
         self.checked = not self.checked
-        if self.click_handler is not None:
-          self.click_handler(self.checked)
-  
-  def clicked(self, pos):
-    return pos[0] > self.x and pos[0] < self.x + self.size and pos[1] > self.y and pos[1] < self.y + self.size
 
 class Button:
   def __init__(self, x, y, width, height, color: tuple, text='', font_size=20, click_handler=None, button=1):
@@ -125,6 +135,10 @@ class UI:
           running = False
         for button in self.buttons:
           button.handle_event(event)
+        for checkbox in self.checkboxs:
+          checkbox.handle_event(event)
+        for text_input in self.text_inputs:
+          text_input.handle_event(event)
 
       self.screen.fill((255, 255, 255))
       for button in self.buttons:
--- example.py
+++ example.py
@@ -3,13 +3,16 @@ from UI_Framework import *
 def hello():
   print('Hello World!')
 
+def handle_text_change(text):
+  print("Text input changed:", text)
+
 ui = UI(800, 600, 'Example')
 button = Button(5, 5, 200, 50, (255, 0, 0), 'Click me!', click_handler=hello)
 text = Text(210, 5, 'Hello World!')
-checkbox = Checkbox(210, 20, 20, (255, 0, 0))
-# text_input = TextInput(100, 200, 200, 50, text='i am confusion')
+checkbox = Checkbox(210, 20, 20, check_color=(0, 0, 0))
+text_input = TextInput(5, 60, 200, 30, placeholder="Type something", on_change=handle_text_change)
 ui.add_button(button)
 ui.add_text(text)
 ui.add_checkbox(checkbox)
-# ui.add_text_input(text_input)
+ui.add_text_input(text_input)
 ui.run()
