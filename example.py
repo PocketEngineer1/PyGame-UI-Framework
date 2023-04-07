ad06d77121cc66d8ab75e93ace274b1ee07f841d
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-04-07 08:20:46 -0500

    Add files via upload

--- example.py
+++ example.py
@@ -4,9 +4,9 @@ def hello():
   print('Hello World!')
 
 ui = UI(800, 600, 'Example')
-button = Button(100, 100, 200, 50, (255, 0, 0), 'Click me!', click_handler=hello)
-text = Text(100, 80, 'Hello World!')
-checkbox = Checkbox(20, 20, 20, (255, 0, 0))
+button = Button(5, 5, 200, 50, (255, 0, 0), 'Click me!', click_handler=hello)
+text = Text(210, 5, 'Hello World!')
+checkbox = Checkbox(210, 20, 20, (255, 0, 0))
 # text_input = TextInput(100, 200, 200, 50, text='i am confusion')
 ui.add_button(button)
 ui.add_text(text)
